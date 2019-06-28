99bfca3cd2284cc14bb5461f735c49ef91531a55
rolfstruikmans <r.struikmans@gmail.com>
2019-06-28 22:57:59 +0200

    hacking in master

--- main.py
+++ main.py
@@ -1,22 +1,17 @@
 # main file
 
 print("hallo wereld")
-print("ik ben nu lekker aan het werk in branch mein_zweig")
-
+print("ik ben nu in de master aan het hacken")
 print("hallo wereld")
-print("ik ben nu lekker aan het werk in branch mein_zweig")
-
+print("ik ben nu in de master aan het hacken")
 print("hallo wereld")
-print("ik ben nu lekker aan het werk in branch mein_zweig")
-
+print("ik ben nu in de master aan het hacken")
 print("hallo wereld")
-print("ik ben nu lekker aan het werk in branch mein_zweig")
-
+print("ik ben nu in de master aan het hacken")
 print("hallo wereld")
-print("ik ben nu lekker aan het werk in branch mein_zweig")
-
+print("ik ben nu in de master aan het hacken")
 print("hallo wereld")
-print("ik ben nu lekker aan het werk in branch mein_zweig")
+print("ik ben nu in de master aan het hacken")
 
 for i in range(0, 10):
     print("hallo wereld")
